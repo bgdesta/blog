9b0a405795d2d3f5bd2f680587587fb92d73ae75
ytbarekg <yhailu@miu.edu>
2022-01-31 15:32:52 -0600

    Discovery Service

---
+++ Discovery/src/main/java/edu/miu/cs544/blog/discover/DiscoveryApplication.java
@@ -0,0 +1,15 @@
+package edu.miu.cs544.blog.discover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+
+}
