9eba42dff3eb1d30629d3a593807e7b66e47904f
Berhanu G. Desta <bertseg@gmail.com>
2022-02-01 10:15:41 -0600

    security dependency added to article and topic

--- topic-service/src/main/resources/import.sql
+++ topic-service/src/main/resources/import.sql
@@ -3,4 +3,3 @@ INSERT INTO TOPIC(id, name, created_at, updated_at) VALUES (101, 'Technology', '
 INSERT INTO TOPIC(id, name, created_at, updated_at) VALUES (102, 'Entertainment', '2022-02-02', '2022-02-02');
 
 INSERT INTO TOPIC(id, name, created_at, updated_at) VALUES (103, 'History', '2022-01-01', '2022-01-01');
-INSERT INTO TOPIC(id, name, created_at, updated_at) VALUES (404, 'Science', '2022-02-02', '2022-02-02');
